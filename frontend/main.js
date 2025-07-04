5c87478d6b541da4a6afd18a34e12b1c3ec05350
Live session user <oscarcs92004@gmail.com>
2025-07-04 20:15:49 -0300

    agrego main.js y cambio index.html

---
+++ frontend/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+	fetch('http://localhost:3000/api/personajes/aleatorio')
+	.then(res => res.json())
+	.then(personaje => {
+		document.getElementById('imagen-personaje').src = personaje.imagen;
+		document.getElementById('nombre-personaje').textContent = personaje.nombre;
+		document.getElementById('serie-personaje').textContent = personaje.origen_id || "Desconocido";
+		document.getElementById('poder-personaje').textContent = personaje.poder;
+		document.getElementById('descripcion-personaje').textContent = personaje.descripcion;
+	      })
+	      .catch(err => {
+		      console.error("Error al cargar el personaje:", err);
+	      });
+});
