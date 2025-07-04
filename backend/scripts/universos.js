7f0da651a9b84712ca70fd8fb3d8f5b03d3108c4
Live session user <oscarcs92004@gmail.com>
2025-07-04 19:05:54 -0300

    agrego CRUD de universos

---
+++ backend/scripts/universos.js
@@ -0,0 +1,68 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'multiversus',
+    password: 'postgres',
+    port: 5432,
+});
+
+async function getAllUniversos()
+{
+    const result = await dbClient.query('SELECT * FROM universos');
+    return result.rows;
+}
+
+async function getOneUniverso(id)
+{
+    const result = await dbClient.query('SELECT * FROM universos WHERE id = $1 LIMIT 1', [id]);    
+    return result.rows[0];
+}
+
+async function createUniverso(nombre, creador, fecha, descripcion)
+{
+    const result = await dbClient.query('INSERT INTO universos (nombre, creador, fecha, descripcion) VALUES ($1, $2, $3, $4) RETURNING *', [nombre, creador, fecha, descripcion]);
+    if(result.rowCount === 0)
+    {
+        return undefined;
+    }
+    else 
+    {
+        return result.rows[0];
+    }
+}
+
+async function deleteUniverso(id)
+{
+    const result = await dbClient.query('DELETE FROM universos WHERE id = $1', [id]);
+    if(result.rowCount === 0)
+    {
+        return undefined;
+    }
+    else
+    {
+        return id;
+    }    
+}
+
+async function updateUniverso(id, nombre, creador, fecha, descripcion)
+{
+    const result = await dbClient.query('UPDATE universos SET nombre = $1, creador = $2, fecha = $3, descripcion = $4 WHERE id = $5 RETURNING *', [nombre, creador, fecha , descripcion, id]);
+    if(result.rowCount === 0)
+    {
+        return undefined;
+    }
+    else
+    {
+        return result.rows[0];
+    }
+}
+
+module.exports = {
+    getAllUniversos,
+    getOneUniverso,
+    createUniverso,
+    deleteUniverso,
+    updateUniverso
+};
