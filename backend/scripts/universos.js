9de1f8e822bbc1c88b1bedaee5aaa4780b05be06
Santiago <sandominguez@fi.uba.ar>
2025-07-04 19:11:29 -0300

    Se modifico la db.sql y las funciones en ./scripts

--- backend/scripts/universos.js
+++ backend/scripts/universos.js
@@ -20,9 +20,9 @@ async function getOneUniverso(id)
     return result.rows[0];
 }
 
-async function createUniverso(nombre, creador, fecha, descripcion)
+async function createUniverso(nombre, creador, fecha, descripcion, pais)
 {
-    const result = await dbClient.query('INSERT INTO universos (nombre, creador, fecha, descripcion) VALUES ($1, $2, $3, $4) RETURNING *', [nombre, creador, fecha, descripcion]);
+    const result = await dbClient.query('INSERT INTO universos (nombre, creador, fecha, descripcion, pais) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, creador, fecha, descripcion, pais]);
     if(result.rowCount === 0)
     {
         return undefined;
@@ -46,9 +46,9 @@ async function deleteUniverso(id)
     }    
 }
 
-async function updateUniverso(id, nombre, creador, fecha, descripcion)
+async function updateUniverso(id, nombre, creador, fecha, descripcion, pais)
 {
-    const result = await dbClient.query('UPDATE universos SET nombre = $1, creador = $2, fecha = $3, descripcion = $4 WHERE id = $5 RETURNING *', [nombre, creador, fecha , descripcion, id]);
+    const result = await dbClient.query('UPDATE universos SET nombre = $1, creador = $2, fecha = $3, descripcion = $4, pais = $5 WHERE id = $6 RETURNING *', [nombre, creador, fecha , descripcion, pais, id]);
     if(result.rowCount === 0)
     {
         return undefined;
