f8ff6353a3aba6c1d90024cf045da638d4b01d09
Santiago <sandominguez@fi.uba.ar>
2025-07-04 17:03:19 -0300

    Se creo un makefile y se modificaron las tablas de la base de datos.

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -11,8 +11,7 @@ create table duelo (
     id serial primary key,
     personaje1_id int REFERENCES personajes (id),
     personaje2_id int REFERENCES personajes (id),
-    votos_personaje1 int default 0,
-    votos_personaje2 int default 0,
+    fecha TIMESTAMP DEFAULT now(),
     ganador_id INT
 )
 
@@ -21,6 +20,5 @@ create table estadisticas (
     duelos_participados int default 0,
     duelos_ganados int default 0,
     duelos_perdidos int default 0,
-    porcentaje_victoria decimal(5,2) default 0.00,
-    imagen varchar(255)
+    porcentaje_victoria decimal(5,2) default 0.00
 )
