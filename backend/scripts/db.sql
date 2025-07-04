9f6fb95085460bda7b312e2b47d31babca729243
Santiago <sandominguez@fi.uba.ar>
2025-07-04 17:44:50 -0300

    Se modificaron las tablas de la base de datos.

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -1,24 +1,37 @@
-create table personajes (
+CREATE TABLE personajes (
     id serial primary key,
-    nombre varchar(100) not null,
-    origen varchar(100) not null,
-    descripcion varchar(255) not null,
-    poder varchar(100) not null,
+    nombre varchar(100) NOT NULL,
+    origen_id int REFERENCES origenes(id),
+    descripcion varchar(255) NOT NULL,
+    poder varchar(100) NOT NULL,
     imagen varchar(255)
 )
 
-create table duelo (
+CREATE TABLE duelo (
     id serial primary key,
     personaje1_id int REFERENCES personajes (id),
     personaje2_id int REFERENCES personajes (id),
     fecha TIMESTAMP DEFAULT now(),
-    ganador_id INT
+    ganador_id int REFERENCES personajes(id),
 )
 
-create table estadisticas (
+CREATE TABLE estadisticas (
     personaje_id int REFERENCES personajes (id),
     duelos_participados int default 0,
     duelos_ganados int default 0,
     duelos_perdidos int default 0,
     porcentaje_victoria decimal(5,2) default 0.00
 )
+
+CREATE TABLE lugares (
+    id serial primary key,
+    nombre varchar(100) NOT NULL UNIQUE,
+    descripcion varchar(255) NOT NULL,
+    origen_id int REFERENCES origenes(id),
+    imagen varchar(255)
+);
+
+CREATE TABLE origenes (
+    id serial primary key,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+);
