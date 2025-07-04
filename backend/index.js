3d05f32b41af5f1a8a060f57b54c7d2f843eee59
Live session user <oscarcs92004@gmail.com>
2025-07-04 20:30:41 -0300

    cambios al backend para poder probar coneccion con frontend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const pool = require('./base_de_datos')
+const pool = require('./base_de_datos');
+const cors = require('cors');
+const personajesRuta = require('./rutas/personajes');
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/test-database', async (req,res) => {
-	try{
+app.use('/api/personajes', personajesRuta);
+
+app.get('/test-database', async (req, res) => {
+  try {
     const result = await pool.query('SELECT NOW()');
     res.json({ success: true, time: result.rows[0].now });
-    } catch (error) {
-    console.error('Error en coneccion', error);
-    res.status(500).json({ success: false, error: error.message});
-    }
+  } catch (error) {
+    console.error('Error en conexión', error);
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 //Get un personaje
