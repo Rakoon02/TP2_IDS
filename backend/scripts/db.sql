8d7f4e2a1ea76355241dbf70a9fc0fdaf95923cb
Oscarcs92004 <oscarcs92004@gmail.com>
2025-07-05 12:51:01 -0300

    Corrigo algunos errores de sintaxis en el script

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -5,16 +5,16 @@ CREATE TABLE personajes (
     descripcion varchar(255) NOT NULL,
     poder varchar(100) NOT NULL,
     imagen varchar(255)
-)
+);
 
 CREATE TABLE duelo (
     id serial primary key,
     personaje1_id int REFERENCES personajes (id),
     personaje2_id int REFERENCES personajes (id),
     fecha TIMESTAMP DEFAULT now(),
-    lugar_id INT REFERENCES lugares(id)
-    ganador_id int REFERENCES personajes(id),
-)
+    lugar_id INT REFERENCES lugares(id),
+    ganador_id int REFERENCES personajes(id)
+);
 
 CREATE TABLE estadisticas (
     personaje_id int REFERENCES personajes (id),
@@ -22,7 +22,7 @@ CREATE TABLE estadisticas (
     duelos_ganados int default 0,
     duelos_perdidos int default 0,
     porcentaje_victoria decimal(5,2) default 0.00
-)
+);
 
 CREATE TABLE lugares (
     id serial primary key,
@@ -37,5 +37,5 @@ CREATE TABLE universos (
     nombre varchar(100) NOT NULL UNIQUE,
     creador varchar(100) NOT NULL,
     fecha varchar(50) NOT NULL,
-    descripcion varchar(255) NOT NULL, 
+    descripcion varchar(255) NOT NULL
 );
