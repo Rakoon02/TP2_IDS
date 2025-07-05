59a42e863ed505cb95cbfc36b1b1325fea82c98b
Live session user <oscarcs92004@gmail.com>
2025-07-05 13:39:52 -0300

    agrego cambios al index.js para visualizar el html

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,14 @@ const PORT = 3000;
 const pool = require('./base_de_datos');
 const cors = require('cors');
 const personajesRuta = require('./rutas/personajes');
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
 
+
 app.use('/api/personajes', personajesRuta);
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/test-database', async (req, res) => {
   try {
@@ -24,6 +27,7 @@ app.get('/test-database', async (req, res) => {
 app.get('/api/personajes/:id', async (req, res) => {
     //res.json({ status: 'OK'})
 });
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 //Get todos los personajes
 app.get('/api/personajes/', async (req, res) => {
