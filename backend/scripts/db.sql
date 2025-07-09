7b16d7ac13f413a379877532397cd0da60142b5a
Oscarcs92004 <oscarcs92004@gmail.com>
2025-07-09 16:43:06 -0300

    agrego cascade para que se borre todo si se borra un universo

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -3,13 +3,14 @@ CREATE TABLE universos (
     nombre varchar(100) NOT NULL UNIQUE,
     creador varchar(100) NOT NULL,
     fecha varchar(50) NOT NULL,
-    descripcion varchar(255) NOT NULL
+    descripcion varchar(255) NOT NULL,
+    imagen varchar(255)
 );
 
 CREATE TABLE personajes (
     id serial primary key,
     nombre varchar(100) NOT NULL,
-    origen_id int REFERENCES universos(id),
+    origen_id int REFERENCES universos(id) ON DELETE CASCADE ON UPDATE CASCADE,
     descripcion varchar(255) NOT NULL,
     poder varchar(100) NOT NULL,
     imagen varchar(255)
@@ -20,25 +21,28 @@ CREATE TABLE lugares (
     id serial primary key,
     nombre varchar(100) NOT NULL UNIQUE,
     descripcion varchar(255) NOT NULL,
-    origen_id int REFERENCES universos(id),
+    origen_id int REFERENCES universos(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    tipo varchar(100),    
     imagen varchar(255)
 );
 
 
 CREATE TABLE duelo (
     id serial primary key,
-    personaje1_id int REFERENCES personajes (id),
-    personaje2_id int REFERENCES personajes (id),
+    personaje1_id int REFERENCES personajes (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    personaje2_id int REFERENCES personajes (id) ON DELETE CASCADE ON UPDATE CASCADE,
     fecha TIMESTAMP DEFAULT now(),
-    lugar_id INT REFERENCES lugares(id),
-    ganador_id int REFERENCES personajes(id)
+    lugar_id INT REFERENCES lugares(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    ganador_id int REFERENCES personajes(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+
 CREATE TABLE estadisticas (
-    personaje_id int REFERENCES personajes (id),
+    personaje_id int REFERENCES personajes (id) ON DELETE CASCADE ON UPDATE CASCADE,
     duelos_participados int default 0,
     duelos_ganados int default 0,
     duelos_perdidos int default 0,
     porcentaje_victoria decimal(5,2) default 0.00
 );
+
 
